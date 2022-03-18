d8c993cd4980df2671edbb70d3ccefc32adb5135
psychopanorama <gregory.gorelik33@gmail.com>
2022-03-18 09:18:20 -0500

    Code updated 3/18/22

---
+++ STEP_1_code.R
@@ -0,0 +1,13 @@
+################## FIRST_RSTUDIO_REPO ############################
+
+## Load Packages----
+library(openxlsx)
+library(readxl)
+library(plyr)
+library(dplyr)
+library(sqldf)
+library(janitor)
+library(tidyverse)
+
+
+List1 <- c(8, 4, 2, 9, 6, 5, 5, 5, 4, 8)
